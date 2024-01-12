29273ee41b4e162d4e99081f5972dcde0ab31d11
Abesh <abeshsarkar2030@gmail.com>
2024-01-12 18:23:26 +0530

    DOM project made

---
+++ script.js
@@ -0,0 +1,11 @@
+let container = document.getElementById("connect");
+let heading = document.getElementById("head");
+
+container.style.width = "300px";
+container.style.height = "200px";
+container.style.backgroundColor = "yellow"
+container.style.margin = "20px";
+container.style.padding = "10px";
+heading.style.fontSize = "18px";
+heading.style.fontWeight = "bold";
+
